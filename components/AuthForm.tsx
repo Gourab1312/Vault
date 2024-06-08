862d4b9f40f823e24facf2a0242b2d85610326ad
Gourab Bandyopadhyay <gourabbandyopadhyay.cgr@gmail.com>
2024-06-08 20:54:19 +0530

    updated the sign in and sign up form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -124,13 +124,13 @@ const AuthForm = ({ type }: { type: string }) => {
                 <>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
-                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name' />
                   </div>
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
                   <div className="flex gap-4">
-                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
-                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: WB' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 712138' />
                   </div>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
